bc3db564d3cf47e5809d088900c18b0e3da072a1
Abdul Waseh <abdul.waseh@virtual-force.com>
2023-06-05 12:04:17 +0500

    have added dynamic baseurl

--- src/App.js
+++ src/App.js
@@ -1,49 +1,74 @@
-import { useState } from 'react';
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [response, setResponse] = useState(null)
-  const [inputText, setInputText] = useState('')
+  const [response, setResponse] = useState(null);
+  const [inputText, setInputText] = useState("");
 
-  const handleSubmit = async(e) => {
-     e.preventDefault()
-
-     let res = await fetch('http://localhost:4000/chatbot', {
-      method: 'post',
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let res = await fetch(`${process.env.REACT_APP_BASEURL}/chatbot`, {
+      method: "post",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body : JSON.stringify({prompt : inputText })
-     })
+      body: JSON.stringify({ prompt: inputText }),
+    });
 
-     let finalResponse = await res.json()
-     setResponse(finalResponse)
+    let finalResponse = await res.json();
+    setResponse(finalResponse);
+  };
 
-  }
-
-  const handleChange =(e) => {
-    setInputText(e.target.value)
-  }
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+  };
 
   return (
     <div className="App">
-    <h3 className='text-center' style={{fontSize : '48px' , marginTop : '50px'}} >Custom ChatGPT</h3>
-  <form style={{marginTop : '80px'}}>
-  <textarea type="text" name="name" class="question" id="nme" required autocomplete="off" value={inputText} onChange = { handleChange } />
-  <label for="nme" style={{fontSize : '28px'}}><span>What's your question?</span></label>
-  <div style={{display : 'flex' , width : '80%' , justifyContent: 'space-between'}}>
-  <div></div>
-  <button onClick={handleSubmit} className="button">Submit</button>
-  </div>
-</form>
+      <h3
+        className="text-center"
+        style={{ fontSize: "48px", marginTop: "50px" }}
+      >
+        Custom ChatGPT
+      </h3>
+      <form style={{ marginTop: "80px" }}>
+        <textarea
+          type="text"
+          name="name"
+          class="question"
+          id="nme"
+          required
+          autocomplete="off"
+          value={inputText}
+          onChange={handleChange}
+        />
+        <label for="nme" style={{ fontSize: "28px" }}>
+          <span>What's your question?</span>
+        </label>
+        <div
+          style={{
+            display: "flex",
+            width: "80%",
+            justifyContent: "space-between",
+          }}
+        >
+          <div></div>
+          <button onClick={handleSubmit} className="button">
+            Submit
+          </button>
+        </div>
+      </form>
 
-    {response && <div>
-      <div style={{marginTop : 100}}>
-       <h3 style={{fontSize : '30px'}} > Response</h3>
-       <p style={{fontSize : '24px', fontWeight: 300}}>{response.message && response.message.text }</p>
-       </div>
-      </div>
-       }
+      {response && (
+        <div>
+          <div style={{ marginTop: 100 }}>
+            <h3 style={{ fontSize: "30px" }}> Response</h3>
+            <p style={{ fontSize: "24px", fontWeight: 300 }}>
+              {response.message && response.message.text}
+            </p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
